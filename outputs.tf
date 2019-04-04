492120a9fe576461a2f52acfc1da942cf8b5a137
05kritika <39408273+05kritika@users.noreply.github.com>
2019-04-04 18:28:21 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,26 @@
+output "address" {
+  value = "${aws_elb.web.dns_name}"
+}
+
+output "instance_id" {
+  value = "${aws_instance.web.id}"
+}
+
+output "vpc_id" {
+  value = "${aws_vpc.default.id}"
+}
+
+output "ami_id" {
+  value = "${aws_instance.web.ami}"
+
+}
+
+output "ec2_security_group" {
+  value = "${aws_security_group.default.name}"
+
+}
+
+output "elb_security_group" {
+  value = "${aws_security_group.elb.name}"
+
+}
